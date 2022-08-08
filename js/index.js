f6b38861bb727793691ac5145ef70ee03e942ab7
Binh <teamgf2002@gmail.com>
2022-08-08 23:30:07 +0700

    responsice

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function vidu() {
+  let bien = document.getElementsByClassName('uudai');
+  console.log('🚀 ~ file: index.js ~ line 3 ~ vidu ~ bien', bien);
+}
+
+vidu();
+
+let myArr = [];
+let i = 0;
+while (i < 5) {
+  myArr.push(i);
+  i++;
+}
+console.log('🚀 ~ file: index.js ~ line 12 ~ myArr', myArr);
